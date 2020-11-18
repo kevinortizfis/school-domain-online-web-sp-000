ac5e3a815a2442dade038afec583d08a424d630c
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-11-18 01:28:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,11 +6,6 @@ class School
   end
 
   def add_student(student,grade)
-      if @roster = {}
-        @roster = {grade => [student]}
-      else
-        @roster[grade][student] = "34"
-      end
   end
 end
 # code here!
