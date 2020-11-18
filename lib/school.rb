5ed5b38c6426795895928d8da0deb080953c583d
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-11-18 00:46:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  attr_reader :grade
+  def initialize(name)
+      @name = name
+      @roster = {}
+  end
+
+  def add_student(student,grade)
+    roster = {grade => []}
+    roster.each do |k,v|
+        if v == []
+        v.push("#{student}")
+      else v.push("secondvalue")
+      end
+    end
+    @roster = roster
+  end
+
+end
