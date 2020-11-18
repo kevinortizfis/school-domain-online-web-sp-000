73f32188cc77e830dadc1685d28942abeabe6b8b
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-11-18 01:42:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
   end
 
   def add_student(student,grade)
-    if @roster[grade] != ["Zach Morris"]
+    if @roster[grade]
       @roster[grade] << student
     else
       @roster[grade] = []
