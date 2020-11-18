bbc4777547ff73b8e4d11316a437b790722017ec
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-11-18 00:51:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,2 @@
+school = School.new("Bayside High School")
 # code here!
-class School
-  attr_accessor :name, :roster
-  attr_reader :grade
-  def initialize(name)
-      @name = name
-      @roster = {}
-  end
-
-  def add_student(student,grade)
-    roster = {grade => []}
-    roster.each do |k,v|
-        if v == []
-        v.push("#{student}")
-      else v.push("secondvalue")
-      end
-    end
-    @roster = roster
-  end
-
-end
