2375ab179ea5a1ee9fe940579b76117f58c89451
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-11-18 01:19:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
 
   def add_student(student,grade)
       @roster = {grade => [student]}
-      grade[student] << student.all
+      grade[student] << student[1]
   end
 end
 # code here!
